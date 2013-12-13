e20b52e3c3421a7ad7b81edf9ecffc748c514434
Alex Honor <alexhonor@yahoo.com>
2013-12-13 13:02:17 -0700

    add method for initialization. add method to get single Image resource. add method to get Instances from ServerArray. add method to get SshKeys for a Cloud.

--- src/main/groovy/com/simplifyops/rundeck/plugin/resources/RightscaleAPI.java
+++ src/main/groovy/com/simplifyops/rundeck/plugin/resources/RightscaleAPI.java
@@ -8,18 +8,22 @@ import java.util.Map;
 
 public interface RightscaleAPI {
 
+    void initialize();
     Map<String, RightscaleResource> getClouds();
     Map<String, RightscaleResource> getDatacenters(String cloud_id);
     Map<String, RightscaleResource> getDeployments();
     Map<String, RightscaleResource> getImages(String cloud_id);
+    RightscaleResource getImage(String href);
     Map<String, RightscaleResource> getInputs(String cloud_id, String instance_id);
     Map<String, RightscaleResource> getInstances(String cloud_id);
     Map<String, RightscaleResource> getInstanceTypes(String cloud_id);
     Map<String, RightscaleResource> getServerArrays();
+    Map<String, RightscaleResource> getServerArrayInstances(String server_array_id);
     Map<String, RightscaleResource> getServers();
     Map<String, RightscaleResource> getServerTemplates();
     Map<String, RightscaleResource> getSubnets(String cloud_id);
+    Map<String, RightscaleResource> getSshKeys(String cloud_id);
+
     Map<String, RightscaleResource> getTags(String href);
-    Map<String, RightscaleResource> getResources(String resourceType);
 
 }
