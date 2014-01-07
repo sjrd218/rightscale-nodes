66d21bf75fe7555b56ec7ee80740cd6cdb5e872b
Alex Honor <alexhonor@yahoo.com>
2014-01-07 15:17:47 -0800

    reformated code.

--- src/test/groovy/com/simplifyops/rundeck/plugin/resources/RightscaleNodesTest.groovy
+++ src/test/groovy/com/simplifyops/rundeck/plugin/resources/RightscaleNodesTest.groovy
@@ -91,8 +91,8 @@ public class RightscaleNodesTest {
     public void createNode() {
         RightscaleNodes rightscaleNodes = new RightscaleNodes(createConfigProperties())
         def node = rightscaleNodes.createNode('node1')
-        Assert.assertEquals("unix",node.getOsFamily())
-        Assert.assertEquals("admin",node.getUsername())
+        Assert.assertEquals("unix", node.getOsFamily())
+        Assert.assertEquals("admin", node.getUsername())
     }
 
     @Test
@@ -101,7 +101,7 @@ public class RightscaleNodesTest {
         try {
             new RightscaleNodes(configuration).validate()
         } catch (ConfigurationException e) {
-             // supposed to throw an exception.
+            // supposed to throw an exception.
         }
         new RightscaleNodes(createConfigProperties()).validate()
     }
@@ -112,66 +112,67 @@ public class RightscaleNodesTest {
         initializeCache(cache)
         def rightscaleNodes = new RightscaleNodes(createConfigProperties())
 
-        def nodeSet =  rightscaleNodes.populateServerNodes(cache)
+        def nodeSet = rightscaleNodes.populateServerNodes(cache)
 
-        Assert.assertEquals(2,nodeSet.getNodes().size())
+        Assert.assertEquals(2, nodeSet.getNodes().size())
         def nodenames = nodeSet.getNodeNames()
-        Assert.assertTrue("name_2149427003 not found. nodes:"+nodenames, nodenames.contains("name_2149427003"))
-        Assert.assertTrue("name_3094109966 not found. nodes:"+nodenames, nodenames.contains("name_3094109966"))
+        Assert.assertTrue("name_2149427003 not found. nodes:" + nodenames, nodenames.contains("name_2149427003"))
+        Assert.assertTrue("name_3094109966 not found. nodes:" + nodenames, nodenames.contains("name_3094109966"))
         def INodeEntry node1 = nodeSet.getNodes().toArray()[0]
         def node1Attrs = node1.getAttributes()
         // server
-        Assert.assertEquals("attrs"+node1Attrs, "operational",node1Attrs['server.state'])
-        Assert.assertEquals("name_2149427003",node1Attrs['server.name'])
-        Assert.assertEquals("description_3746459714",node1Attrs['server.description'])
-        Assert.assertEquals("2013/10/02 01:38:50 +0000",node1Attrs['server.created_at'])
-        Assert.assertEquals("2013/10/02 01:38:50 +0000",node1Attrs['server.updated_at'])
+        Assert.assertEquals("attrs" + node1Attrs, "operational", node1Attrs['server.state'])
+        Assert.assertEquals("name_2149427003", node1Attrs['server.name'])
+        Assert.assertEquals("description_3746459714", node1Attrs['server.description'])
+        Assert.assertEquals("2013/10/02 01:38:50 +0000", node1Attrs['server.created_at'])
+        Assert.assertEquals("2013/10/02 01:38:50 +0000", node1Attrs['server.updated_at'])
         // datacenter
-        Assert.assertEquals("name_1134955039",node1Attrs['datacenter.name'])
-        Assert.assertEquals("description_1627153770",node1Attrs['datacenter.description'])
+        Assert.assertEquals("name_1134955039", node1Attrs['datacenter.name'])
+        Assert.assertEquals("description_1627153770", node1Attrs['datacenter.description'])
         // instance
-        Assert.assertEquals("attrs"+node1Attrs,"name_1680878585",node1Attrs['instance.name'])
-        Assert.assertEquals("resource_3580826380",node1Attrs['instance.resource_uid'])
-        Assert.assertEquals("test_publicdns_3435340970.com",node1Attrs['instance.public_dns_name'])
-        Assert.assertEquals("test_publicdns_3435340970.com",node1Attrs['instance.public_dns_name'])
-        Assert.assertEquals("test_privatedns_2598540039.com",node1Attrs['instance.private_dns_name'])
-        Assert.assertEquals("ud_33741804",node1Attrs['instance.user_data'])
-        Assert.assertEquals("46.34.17.252",node1Attrs['instance.public_ip_address'])
-        Assert.assertEquals("46.34.17.252",node1.getHostname())
-        Assert.assertEquals("6.4.5.3",node1Attrs['instance.private_ip_address'])
-        Assert.assertEquals("fixed",node1Attrs['instance.pricing_type'])
+        Assert.assertEquals("attrs" + node1Attrs, "name_1680878585", node1Attrs['instance.name'])
+        Assert.assertEquals("resource_3580826380", node1Attrs['instance.resource_uid'])
+        Assert.assertEquals("test_publicdns_3435340970.com", node1Attrs['instance.public_dns_name'])
+        Assert.assertEquals("test_publicdns_3435340970.com", node1Attrs['instance.public_dns_name'])
+        Assert.assertEquals("test_privatedns_2598540039.com", node1Attrs['instance.private_dns_name'])
+        Assert.assertEquals("ud_33741804", node1Attrs['instance.user_data'])
+        Assert.assertEquals("46.34.17.252", node1Attrs['instance.public_ip_address'])
+        Assert.assertEquals("46.34.17.252", node1.getHostname())
+        Assert.assertEquals("6.4.5.3", node1Attrs['instance.private_ip_address'])
+        Assert.assertEquals("fixed", node1Attrs['instance.pricing_type'])
         // cloud
-        Assert.assertEquals("Bob's Eucalyptus 125015575",node1Attrs['cloud.display_name'])
-        Assert.assertEquals("eucalyptus",node1Attrs['cloud.cloud_type'])
+        Assert.assertEquals("Bob's Eucalyptus 125015575", node1Attrs['cloud.display_name'])
+        Assert.assertEquals("eucalyptus", node1Attrs['cloud.cloud_type'])
         // server_template
-        Assert.assertEquals("DESCRIPTION_3223207047",node1Attrs['server_template.description'])
-        Assert.assertEquals("NICKNAME_2529968303",node1Attrs['server_template.name'])
-        Assert.assertEquals("0",node1Attrs['server_template.revision'])
+        Assert.assertEquals("DESCRIPTION_3223207047", node1Attrs['server_template.description'])
+        Assert.assertEquals("NICKNAME_2529968303", node1Attrs['server_template.name'])
+        Assert.assertEquals("0", node1Attrs['server_template.revision'])
         // image
-        Assert.assertEquals("resource_machine_2329921984",node1Attrs['image.resource_uid'])
-        Assert.assertEquals("machine",node1Attrs['image.image_type'])
+        Assert.assertEquals("resource_machine_2329921984", node1Attrs['image.resource_uid'])
+        Assert.assertEquals("machine", node1Attrs['image.image_type'])
 
         // input
-        Assert.assertEquals("attrs"+node1Attrs,"text:",node1Attrs['inputs.input_definition_3228327932'])
+        Assert.assertEquals("attrs" + node1Attrs, "text:", node1Attrs['inputs.input_definition_3228327932'])
 
         // ssh_key
-        Assert.assertEquals("attrs"+node1Attrs,"resource_913473243",node1Attrs['ssh_key.resource_uid'])
+        Assert.assertEquals("attrs" + node1Attrs, "resource_913473243", node1Attrs['ssh_key.resource_uid'])
         // tags
         def tags = node1.getTags()
-        Assert.assertEquals("tags="+tags+".",3,tags.size())
+        Assert.assertEquals("tags=" + tags + ".", 3, tags.size())
         Assert.assertTrue("no tag for cloud", tags.contains("Bob's Eucalyptus 125015575"))
-        Assert.assertTrue("no tag for datacenter. tags="+tags, tags.contains("name_1134955039"))
-        Assert.assertTrue("no tag for deployment. tags="+tags, tags.contains("name_3567039744"))
+        Assert.assertTrue("no tag for datacenter. tags=" + tags, tags.contains("name_1134955039"))
+        Assert.assertTrue("no tag for deployment. tags=" + tags, tags.contains("name_3567039744"))
     }
+
     @Test
     public void getServerArrayNodes() {
         def RightscaleBasicCache cache = new RightscaleBasicCache()
         initializeCache(cache)
         def rightscaleNodes = new RightscaleNodes(createConfigProperties())
 
-        def nodeSet =  rightscaleNodes.populateServerArrayNodes(cache)
+        def nodeSet = rightscaleNodes.populateServerArrayNodes(cache)
 
-        Assert.assertEquals(0,nodeSet.getNodes().size())
+        Assert.assertEquals(0, nodeSet.getNodes().size())
 
     }
 
@@ -180,12 +181,12 @@ public class RightscaleNodesTest {
         /**
          * Create API object to simulate the API service.
          */
-        def  query = new RightscaleBasicCache()
+        def query = new RightscaleBasicCache()
         initializeCache(query)
         /**
          * Create an empty cache.
          */
-        def  cache = new RightscaleBasicCache()
+        def cache = new RightscaleBasicCache()
         /**
          * Create the nodes object.
          */
@@ -198,21 +199,20 @@ public class RightscaleNodesTest {
         /**
          * Confirm RightscaleNodes has populated the cache with base data.
          */
-        Assert.assertEquals(query.getClouds().size(),cache.getClouds().size())
-        Assert.assertEquals(query.getDeployments().size(),cache.getDeployments().size())
-        Assert.assertEquals(query.getServers().size(),cache.getServers().size())
-        Assert.assertEquals(query.getServerTemplates().size(),cache.getServerTemplates().size())
-        Assert.assertEquals(query.getServerArrays().size(),cache.getServerArrays().size())
-        cache.getClouds().values().each { cloud ->
-            Assert.assertEquals(3,cache.getDatacenters(cloud.getId()).size())
-            Assert.assertEquals(2,cache.getInstanceTypes(cloud.getId()).size())
-            Assert.assertEquals(2,cache.getSshKeys(cloud.getId()).size())
-            Assert.assertEquals(2,cache.getSubnets(cloud.getId()).size())
-            Assert.assertEquals(2,cache.getInstances(cloud.getId()).size())
-            def instance0 = cache.getInstances(cloud.getId()).values().asList().first()
-            Assert.assertTrue(instance0 instanceof InstanceResource)
-            Assert.assertEquals("name_642936808",instance0.attributes['name'])
-        }
+        Assert.assertEquals(query.getClouds().size(), cache.getClouds().size())
+        Assert.assertEquals(query.getDeployments().size(), cache.getDeployments().size())
+        Assert.assertEquals(query.getServers().size(), cache.getServers().size())
+        Assert.assertEquals(query.getServerTemplates().size(), cache.getServerTemplates().size())
+        Assert.assertEquals(query.getServerArrays().size(), cache.getServerArrays().size())
+
+        def cloud = cache.getClouds().get("/api/clouds/926218062")
+        Assert.assertEquals("wrong num datacenters", 1, cache.getDatacenters(cloud.getId()).size())
+        Assert.assertEquals(1, cache.getInstanceTypes(cloud.getId()).size())
+        Assert.assertEquals(1, cache.getSshKeys(cloud.getId()).size())
+        Assert.assertEquals("wrong num subnets", 2, cache.getSubnets(cloud.getId()).size())
+        Assert.assertEquals(1, cache.getInstances(cloud.getId()).size())
+        def instance0 = cache.getInstances(cloud.getId()).values().asList().first()
+        Assert.assertTrue(instance0 instanceof InstanceResource)
         // TODO: Check more deeply.
     }
 
@@ -223,12 +223,12 @@ public class RightscaleNodesTest {
         /**
          * Create API object to simulate the API service.
          */
-        def  query = new RightscaleBasicCache()
+        def query = new RightscaleBasicCache()
         initializeCache(query)
         /**
          * Create an empty cache.
          */
-        def  cache = new RightscaleBasicCache()
+        def cache = new RightscaleBasicCache()
         /**
          * Create the nodes object.
          */
@@ -238,15 +238,15 @@ public class RightscaleNodesTest {
          */
         def nodeset = nodes.getNodes()
 
-        Assert.assertEquals(2,nodeset.getNodes().size())
-        Assert.assertEquals(false,nodes.needsRefresh())
+        Assert.assertEquals(2, nodeset.getNodes().size())
+        Assert.assertEquals(false, nodes.needsRefresh())
     }
 
-   @Test
+    @Test
 
-     public void filteredInputs() {
+    public void filteredInputs() {
 
-        def  query = new RightscaleBasicCache()
+        def query = new RightscaleBasicCache()
         initializeCache(query)
 
         Properties configuration = createConfigProperties()
